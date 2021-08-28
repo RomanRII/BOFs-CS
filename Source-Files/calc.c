0b518c60e7d5fe3434442eca5be57713205706ad
RomanRII <74742067+RomanRII@users.noreply.github.com>
2021-08-27 22:56:06 -0700

    Create calc.c

---
+++ Source-Files/calc.c
@@ -0,0 +1,11 @@
+include <windows.h>
+#include "beacon.h"
+#include <winbase.h>
+
+/* DECLSPEC_IMPORT WINAPI-OUTPUT WINAPI DLL$FUNCTION(INPUT TYPES); */
+DECLSPEC_IMPORT UINT WINAPI Kernel32$WinExec(LPCSTR, UINT);
+
+void go(char * args, int alen) {
+      BeaconPrintf(CALLBACK_OUTPUT, "Opening calc.exe");
+      Kernel32$WinExec("calc.exe", SW_SHOW);
+}
